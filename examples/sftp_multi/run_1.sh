0938971236a0415a2e4aeba477197e3827ffd1f9
sanghyeonp <sh.austin.park@gmail.com>
2023-04-03 14:19:47 +0900

    correction of the example output

--- examples/sftp_multi/run_1.sh
+++ examples/sftp_multi/run_1.sh
@@ -3,8 +3,8 @@ files=("file1.gz" "file2.gz" "file3.gz" "file4.gz" "file5.gz")
 for idx in ${!files[@]}
 do
 	if [[ $idx == 0 ]]; then
-		echo "/remote/data/share/"
-		echo "/local/example/data"
+		echo "cd /remote/data/share/"
+		echo "lcd /local/example/data"
 	fi
 	echo "get ${files[$idx]}"
 done | sftp USER@example.com
--- examples/sftp_multi/run_2.sh
+++ examples/sftp_multi/run_2.sh
@@ -3,8 +3,8 @@ files=("file6.gz" "file7.gz" "file8.gz" "file9.gz")
 for idx in ${!files[@]}
 do
 	if [[ $idx == 0 ]]; then
-		echo "/remote/data/share/"
-		echo "/local/example/data"
+		echo "cd /remote/data/share/"
+		echo "lcd /local/example/data"
 	fi
 	echo "get ${files[$idx]}"
 done | sftp USER@example.com
