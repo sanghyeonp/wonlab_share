eaed27c8f28db98b38b896602ff235adcc0b978e
sanghyeonp <sh.austin.park@gmail.com>
2023-04-06 17:49:07 +0900

    handle chromosome with prefix 'chr'

--- src/liftover_auto/make_bed.py
+++ src/liftover_auto/make_bed.py
@@ -8,9 +8,10 @@ from packages import *
 
 def make_bed(file, file_compression, delim, snp_col, chr_col, pos_col, outd):
     df_ = pd.read_csv(file, sep=delim, index_col=False, na_values=["NA", "NaN", " "], compression=file_compression)
-
+    df_[chr_col] = df_[chr_col].astype(str)
     df = df_[[chr_col, pos_col, snp_col]]
-    df[chr_col] = df[chr_col].apply(lambda x: "chr{}".format(x))
+    if 'chr' not in df.loc[0, chr_col]:
+        df[chr_col] = df[chr_col].apply(lambda x: "chr{}".format(x))
 
     df['pos-1'] = df[pos_col].apply(lambda x: int(x) - 1)
     df = df[[chr_col, 'pos-1', pos_col, snp_col]]
