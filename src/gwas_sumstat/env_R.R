e5630b01323f40c61457d34526e7d2ee3be3b014
sanghyeonp <sh.austin.park@gmail.com>
2023-04-16 02:26:44 +0900

    save log

--- src/gwas_sumstat/env_R.R
+++ src/gwas_sumstat/env_R.R
@@ -1,10 +1,9 @@
-list.of.packages <- c("argparse", "readr", "dplyr", "tidyr", "rstudioapi")
+list.of.packages <- c("readr", "dplyr", "tidyr", "rstudioapi")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
 
 suppressPackageStartupMessages({
-    require(argparse)
     library(readr)
     library(dplyr)
     library(tidyr)
--- src/gwas_sumstat/gwas_sumstat_ukb_neale.R
+++ src/gwas_sumstat/gwas_sumstat_ukb_neale.R
@@ -1,24 +1,10 @@
+
 ### Load packages from env_R.R
-list.of.packages <- c("tidyverse", "rstudioapi")
+list.of.packages <- c("argparse", "tidyverse", "rstudioapi")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
 
-library(tidyverse)
-getCurrentFileLocation <-  function()
-{
-    this_file <- commandArgs() %>% 
-    tibble::enframe(name = NULL) %>%
-    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
-    dplyr::filter(key == "--file") %>%
-    dplyr::pull(value)
-    if (length(this_file)==0){
-        this_file <- rstudioapi::getSourceEditorContext()$path
-    }
-    return(dirname(this_file))
-}
-
-env_file <- paste(getCurrentFileLocation(), "env_R.R", sep="/")
-source(env_file)
+require(argparse)
 
 ### Common files
 variant_file <- "/data1/sanghyeon/wonlab_contribute/combined/data_common/variants.tsv.bgz"
@@ -52,13 +38,29 @@ parser$add_argument("--delim_out", required=FALSE, default="tab",
 parser$add_argument("--outd", required=FALSE, default="NA",
                     help="Specify the output directory. Default = current working directory.")
 
+### Call packages
+library(tidyverse)
+getCurrentFileLocation <-  function()
+{
+    this_file <- commandArgs() %>% 
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+    if (length(this_file)==0){
+        this_file <- rstudioapi::getSourceEditorContext()$path
+    }
+    return(dirname(this_file))
+}
+
+env_file <- paste(getCurrentFileLocation(), "env_R.R", sep="/")
+source(env_file)
+
 ### Get parser arguments
 args <- parser$parse_args()
 gwas <- args$gwas
 retain_col <- args$retain_col
-if (retain_col[1] == "NA"){
-    stop("Specify the columns to retain using `--retain-col`.")
-}
+
 maf_filter <- args$maf_filter
 keep_no_rsid <- args$keep_no_rsid
 keep_duplicates <- args$keep_duplicates
@@ -68,14 +70,28 @@ rename_col <- args$rename_col
 outf <- args$outf
 delim_out <- args$delim_out
 outd <- args$outd
+if (outf == "NA"){
+    outf <- paste0("reformat.", basename(gwas))
+}
+if (outd == "NA"){
+    outd <- getwd()
+}
+out_path <- paste(outd, outf, sep="/")
+################################################
+log_file <- file(paste0(out_path, ".log"), open = "wt")
+sink(log_file, type = "output")
+
+if (retain_col[1] == "NA"){
+    stop("Specify the columns to retain using `--retain-col`.")
+}
 ################################################
 
 ### Read GWAS summary statistics
-print("::Run:: Reading input GWAS summary statistics")
+cat("\n::Run:: Reading input GWAS summary statistics")
 df <- suppressMessages(suppressWarnings(readr::read_table(gwas, col_names = TRUE)))
 
 ### Read variant to rsID mapping file
-print("::Run:: Reading variant file")
+cat("\n::Run:: Reading variant file")
 df_variant <- suppressMessages(suppressWarnings(readr::read_table(variant_file, col_names = TRUE)))
 df_variant <- subset(df_variant, select=c('variant', 'rsid'))
 colnames(df_variant) <- c("variant", "RSID")
@@ -88,95 +104,87 @@ col_map_dict <- setNames(c('variant', 'minor_allele', 'MAF', 'low_confidence_var
 names(df) <- col_map_dict
 
 ### Obtain CHR, POS, ALT, REF, EAF
-print("::Run:: Extract CHR, POS, REF, ALT and EAF")
+cat("\n::Run:: Extract CHR, POS, REF, ALT and EAF")
 df <- df %>%
     separate(variant, c("CHR", "POS", "REF", "ALT"), sep = ":", convert = TRUE) %>%
     mutate(variant = paste0(CHR, ":", POS, ":", REF, ":", ALT)) %>%
     mutate(EAF = if_else(ALT == minor_allele, MAF, 1 - MAF)) 
 
 ### Map rsID
-print("::Run:: Mapping rsID")
+cat("\n::Run:: Mapping rsID")
 df <- merge(df, df_variant, by='variant')
 nsnp <- nrow(df)
-print(paste0("\tNumber of SNPs before: ", nsnp))
+cat(paste0("\n\tNumber of SNPs before: ", nsnp))
 df2 <- df[grepl("^rs", df$RSID), ]
-print(paste0("\tNumber of SNPs without rsID: ", nsnp - nrow(df2)))
+cat(paste0("\n\tNumber of SNPs without rsID: ", nsnp - nrow(df2)))
 if (keep_no_rsid == FALSE){
     df <- df2
     rm(df2)
-    print(paste0("\tNumber of SNPs after filtering unmapped rsID: ", nrow(df)))
+    cat(paste0("\n\tNumber of SNPs after filtering unmapped rsID: ", nrow(df)))
 }
 
 ### MAF filter
 if (maf_filter != 0){
-    print(paste0("::Run:: MAF filter with threshold: ", maf_filter))
-    print(paste0("\tNumber of SNPs before: ", nrow(df)))
+    cat(paste0("\n::Run:: MAF filter with threshold: ", maf_filter))
+    cat(paste0("\n\tNumber of SNPs before: ", nrow(df)))
     df <- df[df$MAF > maf_filter, ]
-    print(paste0("\tNumber of SNPs after: ", nrow(df)))
+    cat(paste0("\n\tNumber of SNPs after: ", nrow(df)))
 } else{
-    print(paste0("::Run:: No MAF filtering"))
+    cat(paste0("::Run:: No MAF filtering"))
 }
 
 ### Remove duplicates
-print("::Run:: Handle duplicated SNP")
+cat("\n::Run:: Handle duplicated SNP")
 df2 <- df %>%
     distinct(RSID, .keep_all = TRUE)
-print(paste0("\tNumber of SNPs before: ", nrow(df)))
-print(paste0("\tNumber of duplicated SNPs: ", nrow(df) - nrow(df2)))
+cat(paste0("\n\tNumber of SNPs before: ", nrow(df)))
+cat(paste0("\n\tNumber of duplicated SNPs: ", nrow(df) - nrow(df2)))
 if (keep_duplicates == FALSE){
-    print("\tRemoving duplicates...")
+    cat("\n\tRemoving duplicates...")
     df <- df2
     rm(df2)
-    print(paste0("\tNumber of SNPs after: ", nrow(df)))
+    cat(paste0("\n\tNumber of SNPs after: ", nrow(df)))
 } else{
-    print("\tKeeping duplicates...")
+    cat("\n\tKeeping duplicates...")
 }
 
 ### Remove rows with NA
-print("::Run:: Handle rows with NA")
+cat("\n::Run:: Handle rows with NA")
 df2 <- df[complete.cases(df), ]
-print(paste0("\rNumber of rows: ", nrow(df)))
-print(paste0("\rNumber of rows with NA: ", nrow(df) - nrow(df2)))
+cat(paste0("\n\tNumber of rows: ", nrow(df)))
+cat(paste0("\n\tNumber of rows with NA: ", nrow(df) - nrow(df2)))
 if (keep_na == FALSE){
-    print("\tRemoving rows with NA...")
+    cat("\n\tRemoving rows with NA...")
     df <- df2
     rm(df2)
-    print(paste0("\tNumber of rows after: ", nrow(df)))
+    cat(paste0("\n\tNumber of rows after: ", nrow(df)))
 } else{
-    print("\tKeeping rows with NA...")
+    cat("\n\tKeeping rows with NA...")
 }
 
 
 ### Subset specified columns
-print("::Run:: Subset specified columns")
-print(paste0("\tColumns specified: ", paste(retain_col, collapse=", ")))
+cat("\n::Run:: Subset specified columns")
+cat(paste0("\n\tColumns specified: ", paste(retain_col, collapse=", ")))
 
 df <- subset(df, select=as.vector(retain_col))
 
 
 ### Make column name renaming vector
-print("::Run:: Rename the columns")
+cat("\n::Run:: Rename the columns")
 if (rename_col[1] == "NA"){
-    print("\t`--rename-col` not specified. No renaming done.")
+    cat("\n\t`--rename-col` not specified. No renaming done.")
 } else{
     col_new_name <- setNames(rename_col, retain_col)
     names(df) <- col_new_name
 }
 
 ### Save the output
-print("::Run:: Save output GWAS summary statistics")
-if (outf == "NA"){
-    outf <- paste0("reformat.", basename(gwas))
-}
+cat("\n::Run:: Save output GWAS summary statistics")
 
-if (outd == "NA"){
-    outd <- getwd()
-}
+cat(paste0("\n\tSaved at: ", out_path))
 
-out_path <- paste(outd, outf, sep="/")
-print(paste0("\tSaved at: ", out_path))
-
-delim_map <- list(tab = "\t", comma = ",", whitespace = " ")
+delim_map <- list(tab = "\n\t", comma = ",", whitespace = " ")
 
 write.table(df, 
             out_path, 
@@ -184,3 +192,6 @@ write.table(df,
             row.names = FALSE,
             col.names = TRUE,
             quote = FALSE)
+
+sink(type = "output")
+close(log_file)
