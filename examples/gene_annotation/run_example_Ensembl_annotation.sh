cb674cd31da97fa2891bdf4a0b39ef040919583f
sanghyeonp <sh.austin.park@gmail.com>
2023-04-16 20:50:40 +0900

    rename script

--- examples/gene_annotation/run_example_Ensembl_annotation.sh
+++ examples/gene_annotation/run_example_Ensembl_annotation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### Ensembl gene ID to gene symbol, gene chromosome, gene TSS, and gene strand direction (mapping)
-script=../../src/gene_annotation/gene_annotation.py
+script=../../src/gene_annotation/Ensembl_gene_annotation.py
 
 python3 ${script} --file ./data/example_data_ensembl.tsv \
                 --gene-id-col Probe_Ensembl \
