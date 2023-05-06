605ab76d33c5aa73c2aa4828b9b6eaf7b122d6e9
sanghyeonp <sh.austin.park@gmail.com>
2023-05-06 16:11:03 +0900

    .

--- examples/gene_annotation/run_example_Ensembl_annotation.sh
+++ examples/gene_annotation/run_example_Ensembl_annotation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### Ensembl gene ID to gene symbol, gene chromosome, gene TSS, and gene strand direction (mapping)
+# script=/data1/sanghyeon/wonlab_contribute/combined/src/gene_annotation/Ensembl_gene_annotation.py
 script=../../src/gene_annotation/Ensembl_gene_annotation.py
 
 python3 ${script} --file ./data/example_data_ensembl.tsv \
