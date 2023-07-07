776ff74123d3f3bcbd77e76e43c0990996492710
sanghyeonp <sh.austin.park@gmail.com>
2023-07-07 20:44:37 +0900

    added example for single gene query mode

--- examples/gene_annotation/run_example_Ensembl_annotation.sh
+++ examples/gene_annotation/run_example_Ensembl_annotation.sh
@@ -4,6 +4,13 @@
 # script=/data1/sanghyeon/wonlab_contribute/combined/src/gene_annotation/Ensembl_gene_annotation.py
 script=../../src/gene_annotation/Ensembl_gene_annotation.py
 
+# Single gene query mode
+
+python3 ${script} --single-gene \
+                --ensembl-id ENSG00000140675
+
+# Multiple genes query mode
+
 python3 ${script} --file ./data/example_data_ensembl.tsv \
                 --gene-id-col Probe_Ensembl \
                 --delim-in tab \
