19131b50bcd5aa936d0eb11913feb448acfa67cb
sanghyeonp <sh.austin.park@gmail.com>
2024-03-23 05:47:06 +0900

    liftover example using R

--- examples/liftover_auto/run_example.sh
+++ examples/liftover_auto/run_example.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
-script=/data1/sanghyeon/wonlab_contribute/combined/src/liftover_auto/liftover_auto.py
+#SBATCH -J run_example
+#SBATCH -p cpu
+#SBATCH -o ./%x_%j.out
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=14-00:00:00
+#SBATCH --mail-user=sh.austin.park@gmail.com   # 알림 메일 보낼 주소
+#SBATCH --mail-type=END,FAIL              # 알림 메일 타입 (END, FAIL, REQUEUE, TIME_LIMIT_80)
+
+
+### 개인 conda 환경 쓸 때
+module purge
+
+CONDA_PATH=/data1/software/anaconda3         # 콘다 실행 경로
+source $CONDA_PATH/bin/activate /home/sanghyeon/.conda/envs/R4_HPC    # 콘다 환경 실행
+
+
+script=/data1/sanghyeon/wonlab_contribute/combined/src/liftover_auto/liftover_auto.R
 
 #####################
-# Example 1
+# Example 1: Basic
 #####################
-### Chromosome, position, SNP column이 따로 있는 경우.
-python3 ${script} \
-    --file ./data/finngen_R8_T2D.autosome.10k.subset \
+/home/sanghyeon/.conda/envs/R4_HPC/bin/Rscript ${script} \
+    --file-in ./data/finngen_R8_T2D.autosome.10k.subset \
     --delim tab \
     --snp-col rsids \
     --chr-col "#chrom" \
     --pos-col pos \
     --build-from 38 \
     --build-to 37 \
-    --outf example1_out \
-    --verbose
+    --out-pref example1_out \
+    --save-mapping-file
 
 
-#####################
-# Example 2
-#####################
-### Chromosome, position, SNP column이 따로 없고 infer 해야하는 경우.
-# --do-not-save-unlifted: unlifted에 대한 정보는 따로 저장하지 않음.
-# --save-mapping-file: mapping file 따로 저장.
-# --rm-intermediate-file: intermediate file 다 지우기.
-# --drop-pos-build-before: 이전 build 정보들 다 새로운 GWAS에서 지우기.
-python3 ${script} \
-    --file ./data/finngen_R8_T2D.autosome.10k.subset.infer \
-    --delim tab \
-    --infer-col variant CHR:POS:X1:X2 : CHR,POS \
-    --build-from 38 \
-    --build-to 37 \
-    --chr-col-new CHR_b37 \
-    --pos-col-new POS_b37 \
-    --outf example2_out \
-    --verbose \
-    --do-not-save-unlifted \
-    --save-mapping-file \
-    --rm-intermediate-file \
-    --drop-pos-build-before
+
+# ### Chromosome, position, SNP column이 따로 있는 경우.
+# python3 ${script} \
+#     --file ./data/finngen_R8_T2D.autosome.10k.subset \
+#     --delim tab \
+#     --snp-col rsids \
+#     --chr-col "#chrom" \
+#     --pos-col pos \
+#     --build-from 38 \
+#     --build-to 37 \
+#     --outf example1_out \
+#     --verbose
+
+
