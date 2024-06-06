df1237c6dec6a36f2317d9b82c6a902e8f739317
sanghyeonp <sh.austin.park@gmail.com>
2024-06-06 11:40:56 +0900

    code to parse LDSC output

---
+++ src/parse_ldsc_out/fnc.parse_bivariate_ldsc.R
@@ -0,0 +1,28 @@
+# Sanghyeon Park
+# 2024.06.05
+
+parse_bivariate_ldsc <- function(log_file, trait1=NA, trait2=NA){
+    log_content <- readLines(log_file)
+
+    genetic_correlation <- NA; se <- NA; z_score <- NA; p_value <- NA
+    for (line in log_content) {
+        if (grepl("Genetic Correlation:", line)) {
+            # Extract Genetic Correlation
+            query1 <- gsub("Genetic Correlation: ", "", line)
+            genetic_correlation <- str_split(query1, " ")[[1]][1]
+            se <- gsub("\\)", "", gsub("\\(", "", paste(str_split(query1, " ")[[1]][-1], collapse=" ")))
+        }
+        if (grepl("Z-score:", line)) {
+            # Extract Z-score
+            z_score <- gsub("Z-score: ", "", line)
+        }
+        if (grepl("P:", line)) {
+            # Extract P-value
+            p_value <- gsub("P: ", "", line)
+        }
+    }
+    if (is.na(trait1) | is.na(trait2)) {
+        return(data.frame(rg=genetic_correlation, se=se, Z=z_score, P=p_value))
+    } 
+    return(data.frame(trait1=trait1, trait2=trait2, rg=genetic_correlation, se=se, Z=z_score, P=p_value))
+}
---
+++ src/parse_ldsc_out/fnc.parse_univariate_ldsc.R
@@ -0,0 +1,57 @@
+# Sanghyeon Park
+# 2024.06.05
+
+parse_univariate_ldsc <- function(log_file, trait=NA){
+    log_content <- readLines(log_file)
+
+    observed_h2 <- NA; observed_h2_se <- NA; liability_h2 <- NA; liability_h2_se <- NA
+    lambda_gc <- NA; mean_chi_sq <- NA; intercept <- NA; intercept_se <- NA
+    ratio <- NA; ratio_se <- NA
+
+    for (line in log_content) {
+        if (grepl("Total Observed scale h2:", line)) {
+            # Extract Genetic Correlation
+            query1 <- gsub("Total Observed scale h2: ", "", line)
+            observed_h2 <- str_split(query1, " ")[[1]][1]
+            observed_h2_se <- gsub("\\)", "", gsub("\\(", "", paste(str_split(query1, " ")[[1]][-1], collapse=" ")))
+        }
+        if (grepl("Total Liability scale h2:", line)) {
+            # Extract Genetic Correlation
+            query1 <- gsub("Total Liability scale h2: ", "", line)
+            liability_h2 <- str_split(query1, " ")[[1]][1]
+            liability_h2_se <- gsub("\\)", "", gsub("\\(", "", paste(str_split(query1, " ")[[1]][-1], collapse=" ")))
+        }
+        if (grepl("Lambda GC:", line)) {
+            lambda_gc <- gsub("Lambda GC: ", "", line)
+        }
+        if (grepl("Mean Chi^2:", line)) {
+            mean_chi_sq <- gsub("Mean Chi^2: ", "", line)
+        }
+        if (grepl("Intercept:", line)) {
+            query1 <- gsub("Intercept: ", "", line)
+            intercept <- str_split(query1, " ")[[1]][1]
+            intercept_se <- gsub("\\)", "", gsub("\\(", "", paste(str_split(query1, " ")[[1]][-1], collapse=" ")))
+        }
+        if (grepl("Ratio", line)) {
+            if (grepl("Ratio <", line)){
+                ratio <- gsub("\\.", "", gsub("Ratio ", "", line))
+            } else{
+                query1 <- gsub("Ratio: ", "", line)
+                ratio <- str_split(query1, " ")[[1]][1]
+                ratio_se <- gsub("\\)", "", gsub("\\(", "", paste(str_split(query1, " ")[[1]][-1], collapse=" ")))
+            }
+        }
+    }
+    if (is.na(trait)) {
+        return(data.frame(`Observed-scale h2`=observed_h2, `Observed-scale h2 SE`=observed_h2_se,
+                        `Liability-scale h2`=liability_h2, `Liability-scale h2 SE`=liability_h2_se,
+                        `Lambda GC`=lambda_gc, `Mean Chi^2`=mean_chi_sq, 
+                        `Intercept`=intercept, `Intercept SE`=intercept_se,
+                        `Ratio`=ratio, `Ratio SE`=ratio_se))
+    } 
+    return(data.frame(trait=trait, `Observed-scale h2`=observed_h2, `Observed-scale h2 SE`=observed_h2_se,
+                `Liability-scale h2`=liability_h2, `Liability-scale h2 SE`=liability_h2_se,
+                `Lambda GC`=lambda_gc, `Mean Chi^2`=mean_chi_sq, 
+                `Intercept`=intercept, `Intercept SE`=intercept_se,
+                `Ratio`=ratio, `Ratio SE`=ratio_se))
+}
