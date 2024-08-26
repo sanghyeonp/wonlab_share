28c308d40af3d88949d91257ddda8db3bd9a6ec3
sanghyeonp <sh.austin.park@gmail.com>
2024-08-27 00:47:21 +0900

    parse bivariate ldsc intercept

--- src/parse_ldsc_out/fnc.parse_bivariate_ldsc.R
+++ src/parse_ldsc_out/fnc.parse_bivariate_ldsc.R
@@ -6,7 +6,8 @@ library(stringr)
 parse_bivariate_ldsc <- function(log_file, trait1=NA, trait2=NA){
     log_content <- readLines(log_file)
 
-    genetic_correlation <- NA; se <- NA; z_score <- NA; p_value <- NA
+    genetic_correlation <- NA; se <- NA; z_score <- NA; p_value <- NA; bivar_intercept <- NA; bivar_intercept_se <- NA
+    keep_next <- FALSE
     for (line in log_content) {
         if (grepl("Genetic Correlation:", line)) {
             # Extract Genetic Correlation
@@ -22,9 +23,20 @@ parse_bivariate_ldsc <- function(log_file, trait1=NA, trait2=NA){
             # Extract P-value
             p_value <- gsub("P: ", "", line)
         }
+        if (keep_next){
+            # Extract bivariate intercept
+            query1 <- gsub("Intercept: ", "", line)
+            bivar_intercept <- str_split(query1, " ")[[1]][1]
+            bivar_intercept_se <- gsub("\\)", "", gsub("\\(", "", paste(str_split(query1, " ")[[1]][-1], collapse=" ")))
+            keep_next <- FALSE
+        }
+        if (grepl("Mean z1", line)){
+            # Capture Mean z1*z2 line
+            keep_next <- TRUE
+        }
     }
     if (is.na(trait1) | is.na(trait2)) {
-        return(data.frame(rg=genetic_correlation, se=se, Z=z_score, P=p_value))
+        return(data.frame(rg=genetic_correlation, se=se, Z=z_score, P=p_value, intercept=bivar_intercept, intercept_se=bivar_intercept_se))
     } 
-    return(data.frame(trait1=trait1, trait2=trait2, rg=genetic_correlation, se=se, Z=z_score, P=p_value))
+    return(data.frame(trait1=trait1, trait2=trait2, rg=genetic_correlation, se=se, Z=z_score, P=p_value, intercept=bivar_intercept, intercept_se=bivar_intercept_se))
 }
